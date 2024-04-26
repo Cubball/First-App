92c51e9cf33a9be7fdee5ad2edc318734e6e660d
Cubball <138378823+Cubball@users.noreply.github.com>
2024-04-26 14:32:57 +0300

    Register DbContext in Program.cs

--- TaskBoard.API/Program.cs
+++ TaskBoard.API/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using TaskBoard.DAL.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<TaskBoardDbContext>(o => o.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
