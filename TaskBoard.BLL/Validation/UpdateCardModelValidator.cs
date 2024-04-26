f508556576d87e206a7ebd9ca984363c437126ee
Cubball <138378823+Cubball@users.noreply.github.com>
2024-04-26 14:03:39 +0300

    Add validators for CreateCardModel and UpdateCardModel

---
+++ TaskBoard.BLL/Validation/UpdateCardModelValidator.cs
@@ -0,0 +1,23 @@
+using FluentValidation;
+using TaskBoard.BLL.Infrastructure;
+using TaskBoard.BLL.Models;
+
+namespace TaskBoard.BLL.Validation;
+
+public class UpdateCardModelValidator : AbstractValidator<UpdateCardModel>
+{
+    public UpdateCardModelValidator(IDateTimeProvider dateTimeProvider)
+    {
+        RuleFor(c => c.Name)
+            .NotNull()
+            .NotEmpty()
+            .Length(2, 100);
+        RuleFor(c => c.Description)
+            .NotNull()
+            .MaximumLength(1000);
+        RuleFor(c => c.DueDate)
+            .InTheFuture(dateTimeProvider);
+        RuleFor(c => c.Priority)
+            .ValidPriority();
+    }
+}
