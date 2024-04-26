bc9b862473cb1a199a76261f5126f93ca830c162
Cubball <138378823+Cubball@users.noreply.github.com>
2024-04-26 16:50:56 +0300

    Add IListService to the DI container and map list endpoints

--- TaskBoard.API/Program.cs
+++ TaskBoard.API/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddDbContext<TaskBoardDbContext>(o => o.UseNpgsql(connectionStr
 builder.Services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
 builder.Services.AddValidatorsFromAssemblyContaining<IAssemblyMarker>();
 builder.Services.AddScoped<ICardService, CardService>();
+builder.Services.AddScoped<IListService, ListService>();
 builder.Services.AddScoped<DbInitializer>();
 
 var app = builder.Build();
@@ -21,5 +22,7 @@ var dbInitializer = scope.ServiceProvider.GetRequiredService<DbInitializer>();
 await dbInitializer.InitializeAsync();
 
 app.MapCardEndpoints();
+
+app.MapListEndpoints();
 
 app.Run();
