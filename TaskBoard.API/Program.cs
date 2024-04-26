445931b2a6b2d86e1a42c044d2905c1428b213c2
Cubball <138378823+Cubball@users.noreply.github.com>
2024-04-26 14:41:56 +0300

    Register the rest of the services

--- TaskBoard.API/Program.cs
+++ TaskBoard.API/Program.cs
@@ -1,10 +1,17 @@
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
+using TaskBoard.API.Endpoints;
+using TaskBoard.BLL.Infrastructure;
+using TaskBoard.BLL.Services;
 using TaskBoard.DAL.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<TaskBoardDbContext>(o => o.UseNpgsql(connectionString));
+builder.Services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+builder.Services.AddValidatorsFromAssemblyContaining<IAssemblyMarker>();
+builder.Services.AddScoped<ICardService, CardService>();
 builder.Services.AddScoped<DbInitializer>();
 
 var app = builder.Build();
