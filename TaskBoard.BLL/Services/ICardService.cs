41ecec9eb5daf189f9dab7c23e069e226dce47be
Cubball <138378823+Cubball@users.noreply.github.com>
2024-04-26 13:46:27 +0300

    Add ICardService

---
+++ TaskBoard.BLL/Services/ICardService.cs
@@ -0,0 +1,17 @@
+using ErrorOr;
+using TaskBoard.BLL.Models;
+
+namespace TaskBoard.BLL.Services;
+
+public interface ICardService
+{
+    Task<IEnumerable<ListWithCardsModel>> GetAllCardsAsync();
+
+    Task<ErrorOr<CardFullModel>> GetCardByIdAsync(int id);
+
+    Task<ErrorOr<CardFullModel>> CreateCardAsync(CreateCardModel cardModel);
+
+    Task<ErrorOr<Updated>> UpdateCardAsync(UpdateCardModel cardModel);
+
+    Task<ErrorOr<Deleted>> DeleteCardByIdAsync(int id);
+}
