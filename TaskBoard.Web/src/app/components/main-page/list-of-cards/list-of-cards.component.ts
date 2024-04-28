6f3a6c3a9fe8d4a33878d844d14515f6c28a89d6
Cubball <138378823+Cubball@users.noreply.github.com>
2024-04-28 12:32:01 +0300

    Style card lists with flex

--- TaskBoard.Web/src/app/components/main-page/list-of-cards/list-of-cards.component.ts
+++ TaskBoard.Web/src/app/components/main-page/list-of-cards/list-of-cards.component.ts
@@ -2,13 +2,19 @@ import { Component, Input } from '@angular/core';
 import { ListWithCards } from '../../../types/list-with-cards';
 import { CardComponent } from '../card/card.component';
 import { CommonModule } from '@angular/common';
+import { faEllipsisVertical, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-list-of-cards',
   standalone: true,
-  imports: [CardComponent, CommonModule],
+  imports: [CardComponent, CommonModule, FontAwesomeModule],
   templateUrl: './list-of-cards.component.html',
 })
 export class ListOfCardsComponent {
+  faEllipsisVertical = faEllipsisVertical;
+  faPlus = faPlus;
   @Input() list!: ListWithCards;
+
+  onAddNewCardClick() { }
 }
